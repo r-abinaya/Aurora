65604cafd3507680f4b71b6d595cf916cde2037c
aloaksh <akshayaaloshious@gmail.com>
2023-03-31 14:22:13 +0530

    Default app deleted

---
+++ aurora/lib/card.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+import 'card_class.dart';
+
+class Card extends StatelessWidget {
+  final CardWidget widgetList;
+  const Card ({super.key, required this.widgetList});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
---
+++ aurora/lib/card_class.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CardWidget{
+  // ignore: non_constant_identifier_names
+  var WidgetList = <Widget>[];
+}
