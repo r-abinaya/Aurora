8831debfd1f57c7ad99a0ab097d9ee166a21ead3
Kaviyadh <gkaviyadharshini1202@gmail.com>
2023-04-01 13:17:08 +0530

    Firebase connect code

--- aurora/lib/pages/card.dart
+++ aurora/lib/pages/card.dart
@@ -7,8 +7,8 @@ class Card extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    return Column(
+      
     );
   }
 }
