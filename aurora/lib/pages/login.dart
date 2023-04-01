53cc64d1585e0729c53951164fc37fc6a5df777c
aloaksh <akshayaaloshious@gmail.com>
2023-04-01 23:39:44 +0530

    Stateful widget for login signup

--- aurora/lib/pages/login.dart
+++ aurora/lib/pages/login.dart
@@ -1,9 +1,14 @@
 import "package:aurora/pages/pages.dart";
 import "package:flutter/material.dart";
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
   const LogIn({super.key});
 
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- aurora/lib/pages/signup.dart
+++ aurora/lib/pages/signup.dart
@@ -1,9 +1,15 @@
 import "package:aurora/pages/pages.dart";
 import "package:flutter/material.dart";
+import 'package:firebase_auth/firebase_auth.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   const SignUp({super.key});
 
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
