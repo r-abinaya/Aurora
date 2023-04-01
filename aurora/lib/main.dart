ef1f6ef28fa1cfd0a314849f4e28a7ef6e1b12f2
Kaviyadh <gkaviyadharshini1202@gmail.com>
2023-04-02 05:16:28 +0530

    nothing

--- aurora/lib/main.dart
+++ aurora/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:aurora/pages/pages.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
 
   // This widget is the root of your application.
   @override
--- aurora/lib/pages/donateRestDet.dart
+++ aurora/lib/pages/donateRestDet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+
 
 class DonateDet extends StatefulWidget {
   const DonateDet({super.key});
@@ -11,25 +14,38 @@ class _ProfileState extends State<DonateDet> {
   String name = '';
   String location = '';
   String dietPreference = '';
-  
+  final userfdetController = TextEditingController();
+  final userqntyController = TextEditingController();
+  final usernopController = TextEditingController();
+  final userCookController = TextEditingController();
+  final userexpController = TextEditingController();
+  final userdesController = TextEditingController();
+  late DatabaseReference dbef  = FirebaseDatabase.instance.ref().child('Food');
+
+  //dbef = FirebaseDatabase.instance.ref().child('Food');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          onPressed: (){},
+          onPressed: () {},
           icon: const Icon(Icons.arrow_back_ios_new),
-          ),
-        title:  const Text('Enter Food Details',),
-        actions: const[],
-        backgroundColor:const Color.fromARGB(255, 110, 212, 159),
+        ),
+        title: const Text(
+          'Enter Food Details',
+        ),
+        actions: const [],
+        backgroundColor: const Color.fromARGB(255, 110, 212, 159),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+         
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
+              
               'Food Serve Quantity(No of people)',
               style: TextStyle(
                 fontSize: 18.0,
@@ -38,6 +54,8 @@ class _ProfileState extends State<DonateDet> {
             ),
             const SizedBox(height: 8.0),
             TextField(
+  controller:userqntyController ,
+
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Enter food quantity',
@@ -58,6 +76,7 @@ class _ProfileState extends State<DonateDet> {
             ),
             const SizedBox(height: 8.0),
             TextField(
+               controller:userCookController,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Enter cooked time',
@@ -69,7 +88,7 @@ class _ProfileState extends State<DonateDet> {
               },
             ),
             const SizedBox(height: 16.0),
-             const Text(
+            const Text(
               'Food Expiry Time',
               style: TextStyle(
                 fontSize: 18.0,
@@ -78,6 +97,7 @@ class _ProfileState extends State<DonateDet> {
             ),
             const SizedBox(height: 8.0),
             TextField(
+               controller:userexpController,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Enter expiry time',
@@ -98,6 +118,7 @@ class _ProfileState extends State<DonateDet> {
             ),
             const SizedBox(height: 8.0),
             TextField(
+               controller:userdesController,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Eg:Rice,sambar,etc..',
@@ -109,15 +130,22 @@ class _ProfileState extends State<DonateDet> {
               },
             ),
             const SizedBox(height: 16.0),
-          
-            
             Center(
               child: ElevatedButton(
                 style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(47, 155, 142, 1)),
+                  backgroundColor: MaterialStateProperty.all(
+                      const Color.fromRGBO(47, 155, 142, 1)),
                 ),
                 child: const Text('Save'),
                 onPressed: () {
+                  Map<String,String> food = {
+                    'des':userdesController.text,
+                    'qnty':userqntyController.text,
+                    'cook':userCookController.text,
+                    'exp':userexpController.text
+                  };
+                  dbef.push().set(food);
+
                   // Save profile information here
                 },
               ),
